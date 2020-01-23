8dfb7c579e156967c330a01bebc850d0964e447c
Yamil Arroliga <arroliga.1059@gmail.com>
2020-01-23 11:59:42 -0600

    AutorService

---
+++ API_Noticiero_NetCore/API_Noticiero_NetCore/Controllers/AutorController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Noticiero_NetCore.Models;
+using API_Noticiero_NetCore.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Noticiero_NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        public readonly AutorService _autorservice;
+        public AutorController(AutorService autorService)
+        {
+            _autorservice = autorService;
+        }
+
+        [HttpGet]
+        [Route("Mostrar")]
+        public IActionResult MostrarListado()
+        {
+            var resultado = _autorservice.ListarAutores();
+            return Ok(resultado);           
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult GuardarAutor([FromBody] Autor _autor)
+        {
+            var resultado = _autorservice.AgregarAutor(_autor);
+            if (resultado)
+            {
+                return Ok(resultado);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpPut]
+        [Route("Modificar")]
+        public IActionResult ModificarAutor([FromBody] Autor _autor)
+        {
+            var resultado = _autorservice.EditarAutor(_autor);
+            if (resultado)
+            {
+                return Ok(resultado);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete]
+        [Route("Eliminar")]
+        public IActionResult EliminarAutor(Autor _autor)
+        {
+            var resultado = _autorservice.EliminarAutor(_autor);
+            if (resultado)
+            {
+                return Ok(resultado);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ API_Noticiero_NetCore/API_Noticiero_NetCore/Servicios/AutorService.cs
@@ -0,0 +1,66 @@
+﻿using API_Noticiero_NetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Noticiero_NetCore.Servicios
+{
+    public class AutorService
+    {
+        private readonly NoticieroDbContext _NoticieroDbContext;
+
+        public AutorService(NoticieroDbContext noticieroDbContext)
+        {
+            _NoticieroDbContext = noticieroDbContext;
+        }
+        public List<Autor> ListarAutores()
+        {
+            var resultado = _NoticieroDbContext.autor.ToList();
+            return resultado;
+        }
+
+        public Boolean AgregarAutor(Autor _autor)
+        {
+            try
+            {
+                _NoticieroDbContext.autor.Add(_autor);
+                _NoticieroDbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Boolean EditarAutor(Autor _autor)
+        {
+            try
+            {
+                _NoticieroDbContext.Entry(_autor).State = EntityState.Modified;
+                _NoticieroDbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Boolean EliminarAutor(Autor _autor)
+        {
+            try
+            {
+                _NoticieroDbContext.Entry(_autor).State = EntityState.Deleted;
+                _NoticieroDbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
